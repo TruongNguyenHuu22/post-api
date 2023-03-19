31d255fb3384c83e9c0151a8dea854a22bcc3b6a
HenryNguyen <nhtruong2@gmail.com>
2023-03-19 11:45:42 +0700

    api test

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/greeting', function () {
+    return new JsonResponse(['message' => 'hello world']);
 });
+
+Route::get(
+    '/users',
+    [UserController::class, 'index']
+);
